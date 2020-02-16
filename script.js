a506839e804d74acf921fb4d18eea2778dcdf87c
Corey Hedberg <chedberg@gmail.com>
2020-02-16 09:31:20 -0500

    Initial commit.

---
+++ script.js
@@ -0,0 +1,7 @@
+/*
+  This website was written by Corey Hedberg for the purposes of the Free Code Camp Responsive Web Design Certification "Build a Survey Form" challenge.
+*/
+
+function submitSurvey() {
+	alert('Thank you for completing this survey. Have a great day!');
+}
